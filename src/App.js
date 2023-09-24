a0c688ac15ad30f5755f2b8cf69e1b6af8aad8be
haokexin <haokexin02@corp.netease.com>
2023-09-25 00:32:23 +0800

    add sjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import * as GC from '@grapecity/spread-sheets';
+import { SpreadSheets, Worksheet } from '@grapecity/spread-sheets-react';
+import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
 
 function App() {
+  let hostStyle = {
+    width: '100%',
+    height: '600px',
+    border: '1px solid darkgray',
+  };
+
+  let initSpread = function(spread) {
+    let sheet = spread.getActiveSheet();
+    sheet.getCell(0, 0).vAlign(GC.Spread.Sheets.VerticalAlign.center).value('Hello SpreadJS!');
+
+
+    //Setting Values - Text
+    sheet.setValue(1, 1, 'Setting Values');
+    //Setting Values - Number
+    sheet.setValue(2, 1, 'Number');
+    sheet.setValue(2, 2, 23);
+    sheet.setValue(3, 1, 'Text');
+    sheet.setValue(3, 2, 'GrapeCity');
+    sheet.setValue(4, 1, 'Datetime');
+    //Setting Values - DateTime
+    sheet.getCell(4, 2).value(new Date(2020, 10, 7)).formatter('mm-dd-yyyy');
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <SpreadSheets workbookInitialized={spread => initSpread(spread)} hostStyle={hostStyle}>
+        <Worksheet />
+      </SpreadSheets>
     </div>
   );
 }
