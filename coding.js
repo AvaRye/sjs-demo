cb13d4dc578f5a6199b5717e15894f4fbd499959
haokexin <haokexin02@corp.netease.com>
2023-09-25 15:27:52 +0800

    coding solve

--- coding.js
+++ coding.js
@@ -15,6 +15,24 @@ const merge1 = (nums1, m, nums2, n) => {
 };
 
 const merge2 = (nums1, m, nums2, n) => {
+  // 优化流程，使用temp记录最新插入位置，避免重复对比
+  let temp = 0;
+  for (let i = 0; i < n; i++) { // 第二个数组
+    for (let j = temp; j < m; j++) { // 第一个数组
+      if (j === m - 1) { // 如果到达了第一个数组最后一个还没有插入就push到最后
+        nums1.push(nums2[i]);
+        break;
+      }
+      if (nums2[i] <= nums1[j]) {
+        nums1.splice(j, 0, nums2[i]); // 不大于时插入
+        temp = j;
+        break;
+      }
+    }
+  }
+};
+
+const merge3 = (nums1, m, nums2, n) => {
   // 如果不限制必须严格操作第一个数组的话可以更简单
   let temp = nums1.concat(nums2).sort((a, b) => a - b);
   nums1.splice(0, m, ...temp);
@@ -24,5 +42,5 @@ const merge2 = (nums1, m, nums2, n) => {
 let num1 = [1, 5, 7, 8];
 let num2 = [0, 2, 3, 3, 4, 9, 10, 11];
 
-merge1(num1, num1.length, num2, num2.length);
+merge2(num1, num1.length, num2, num2.length);
 console.log('merged num1', num1);
